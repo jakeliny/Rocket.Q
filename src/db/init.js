53ef7a5c305d6fad6c0d0fc0a992dfae6683cf91
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-06-25 13:51:51 -0300

    aula 1 a 5

---
+++ src/db/init.js
@@ -0,0 +1,27 @@
+const Database = require("./config")
+
+const initDb = {
+    async init(){
+        const db = await Database()
+
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
+
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`);
+
+        await db.close()
+    }
+}
+
+initDb.init();
+
+
+
+
---
+++ src/route.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
+
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
+
+module.exports = route
